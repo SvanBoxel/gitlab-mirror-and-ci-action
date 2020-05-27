2ec0ff8c6140204c18a2802fa0061f945d6ffae8
Daniel Arndt <arndtd@ornl.gov>
2020-05-27 15:56:37 -0400

    Prepend branch name with repository name

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ POLL_TIMEOUT=${POLL_TIMEOUT:-$DEFAULT_POLL_TIMEOUT}
 
 git checkout "${GITHUB_REF:11}"
 
-branch=$(git symbolic-ref --short HEAD)
+branch=${GITHUB_REPOSITORY}/$(git symbolic-ref --short HEAD)
 
 sh -c "git config --global credential.username $GITLAB_USERNAME"
 sh -c "git config --global core.askPass /cred-helper.sh"
@@ -17,6 +17,9 @@ sh -c "echo pushing to $branch branch at $(git remote get-url --push mirror)"
 sh -c "git push mirror $branch"
 
 sleep $POLL_TIMEOUT
+
+# convert slashes in a HTML-compatible way
+branch=${branch//\//%2F}
 
 pipeline_id=$(curl --header "PRIVATE-TOKEN: $GITLAB_PASSWORD" --silent "https://${GITLAB_HOSTNAME}/api/v4/projects/${GITLAB_PROJECT_ID}/repository/commits/${branch}" | jq '.last_pipeline.id')
 
