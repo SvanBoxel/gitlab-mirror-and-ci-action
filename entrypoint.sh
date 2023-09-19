0001c7603575b9498196345cb3ff56cb3f6094f8
Piotr Klinke <51191450+klinkeklinke@users.noreply.github.com>
2023-09-19 15:35:27 +0200

    Specify safe directory to mitigate dubious ownership issue

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,6 +27,7 @@ branch="$(git symbolic-ref --short HEAD)"
 branch_uri="$(urlencode ${branch})"
 
 sh -c "git config --global credential.username $GITLAB_USERNAME"
+sh -c "git config --global --add safe.directory /github/workspace"
 sh -c "git config --global core.askPass /cred-helper.sh"
 sh -c "git config --global credential.helper cache"
 sh -c "git remote add mirror $*"
