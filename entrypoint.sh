700da351ea9e5fa5f5d50bec9d25f7eff09b88f6
Piotr Klinke <51191450+klinkeklinke@users.noreply.github.com>
2023-09-20 22:31:17 +0200

    Add safe directory moved to the beginning

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,13 +21,13 @@ urlencode() (
 DEFAULT_POLL_TIMEOUT=10
 POLL_TIMEOUT=${POLL_TIMEOUT:-$DEFAULT_POLL_TIMEOUT}
 
+sh -c "git config --global --add safe.directory /github/workspace"
 git checkout "${GITHUB_REF:11}"
 
 branch="$(git symbolic-ref --short HEAD)"
 branch_uri="$(urlencode ${branch})"
 
 sh -c "git config --global credential.username $GITLAB_USERNAME"
-sh -c "git config --global --add safe.directory /github/workspace"
 sh -c "git config --global core.askPass /cred-helper.sh"
 sh -c "git config --global credential.helper cache"
 sh -c "git remote add mirror $*"
